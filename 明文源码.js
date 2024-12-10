434a40716f96e85110d62ac15bc9cf1c1e0bddc0
TyrEamon <yuanbin200409@gmail.com>
2024-12-11 01:44:17 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '62bf390c-a0f9-4b56-89ca-4544428c446a';
 
-const proxyIPs = ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'workers.cloudflare.cyou'];
+const proxyIPs = [ 'usfd.tyreamon.us.kg'];
 
 // if you want to use ipv6 or single proxyIP, please add comment at this line and remove comment at the next line
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
